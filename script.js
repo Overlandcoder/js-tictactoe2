cfc2e540d61c7f2dd8123883d92068401d9868b8
Overlander <anuragnandal@hotmail.com>
2025-12-09 09:07:46 -0500

    Refactor how symbols are assigned to players

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const gameBoard = (function () {
+  const boardArray = [];
+
+  return { boardArray };
+})();
+
 function createPlayer(name, symbol) {
   return { name, symbol };
 }
@@ -6,16 +12,20 @@ Array.prototype.random = function () {
   return this[Math.floor(Math.random() * this.length)];
 };
 
-const assignOtherSymbol = (symbol) => (symbol === "X" ? "O" : "X");
 
 const game = (function () {
   const player1 = createPlayer("A");
   const player2 = createPlayer("B");
-  player1.symbol = assignRandomSymbol();
-  player2.symbol = assignOtherSymbol(player1.symbol);
+  const assignOtherSymbol = (symbol) => (symbol === "X" ? "O" : "X");
+  assignSymbols()
 
   function assignRandomSymbol() {
     const symbols = ["X", "O"];
     return symbols.random();
+  }
+
+  function assignSymbols() {
+    player1.symbol = assignRandomSymbol();
+    player2.symbol = assignOtherSymbol(player1.symbol);
   }
 })();
