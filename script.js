d6e17c7525609a78bb056a2ecac94e8d70984462
Overlander <anuragnandal@hotmail.com>
2025-12-12 15:27:48 -0500

    Add ability to place symbols and change turns

--- script.js
+++ script.js
@@ -14,20 +14,31 @@ function createPlayer(name, symbol) {
 
 const gameDisplay = (function() {
   function announceSymbols(player1Symbol, player2Symbol) {
-    const symbolsInfoDiv = document.querySelector(".symbols-info")
+    const symbolsInfoDiv = document.querySelector("#symbols-info")
     symbolsInfoDiv.textContent = `Player 1 is ${player1Symbol},
                                   Player 2 is ${player2Symbol}.`
   }
 
-  return { announceSymbols }
+  function announceTurn(playerName) {
+    const turnInfoDiv = document.querySelector("#turn-info")
+    turnInfoDiv.textContent = `${playerName}, it's your turn.`
+  }
+
+  function placeSymbol(cell, symbol) {
+    cell.textContent = symbol
+  }
+
+  return { announceSymbols, announceTurn, placeSymbol }
 })()
 
 const game = (function () {
-  const player1 = createPlayer("A");
-  const player2 = createPlayer("B");
+  const player1 = createPlayer("Player 1");
+  const player2 = createPlayer("Player 2");
   const assignOtherSymbol = (symbol) => (symbol === "X" ? "O" : "X");
   const playButton = document.querySelector("#play")
   playButton.addEventListener("click", play)
+  const chooseRandomPlayer = () => [player1, player2].random()
+  let currentPlayer = chooseRandomPlayer()
 
   function assignRandomSymbol() {
     const symbols = ["X", "O"];
@@ -43,6 +54,26 @@ const game = (function () {
   function play() {
     assignSymbols()
     gameDisplay.announceSymbols(player1.symbol, player2.symbol)
-    
+    gameDisplay.announceTurn(currentPlayer.name)
+    const cells = document.querySelectorAll(".cell")
+    addListenersToCells(cells)
   }
+
+  function addListenersToCells(cells, symbol) {
+    cells.forEach(cell => cell.addEventListener("click", () => handleMove(cell), { once: true }))
+  }
+
+  function handleMove(cell) {
+    addSymbolToBoardArray(cell.id)
+    gameDisplay.placeSymbol(cell, currentPlayer.symbol)
+    currentPlayer = switchTurn()
+  }
+
+  function addSymbolToBoardArray(index) {
+    gameBoard.boardArray[index] = currentPlayer.symbol
+    console.log(gameBoard.boardArray)
+  }
+
+  const switchTurn = () => currentPlayer === player1 ? player2 : player1
+    
 })();
