c383b089d1d3c9c8b0f1824f16ddccf15920a80c
Overlander <anuragnandal@hotmail.com>
2025-12-12 13:47:56 -0500

    Announce assigned symbols when new game starts

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+Array.prototype.random = function () {
+  return this[Math.floor(Math.random() * this.length)];
+};
+
 const gameBoard = (function () {
   const boardArray = [];
 
@@ -8,24 +12,37 @@ function createPlayer(name, symbol) {
   return { name, symbol };
 }
 
-Array.prototype.random = function () {
-  return this[Math.floor(Math.random() * this.length)];
-};
+const gameDisplay = (function() {
+  function announceSymbols(player1Symbol, player2Symbol) {
+    const symbolsInfoDiv = document.querySelector(".symbols-info")
+    symbolsInfoDiv.textContent = `Player 1 is ${player1Symbol},
+                                  Player 2 is ${player2Symbol}.`
+  }
 
+  return { announceSymbols }
+})()
 
 const game = (function () {
   const player1 = createPlayer("A");
   const player2 = createPlayer("B");
   const assignOtherSymbol = (symbol) => (symbol === "X" ? "O" : "X");
-  assignSymbols()
+  const playButton = document.querySelector("#play")
+  playButton.addEventListener("click", play)
 
   function assignRandomSymbol() {
     const symbols = ["X", "O"];
     return symbols.random();
+    // refactor via arrow func
   }
 
   function assignSymbols() {
     player1.symbol = assignRandomSymbol();
     player2.symbol = assignOtherSymbol(player1.symbol);
+  }
+
+  function play() {
+    assignSymbols()
+    gameDisplay.announceSymbols(player1.symbol, player2.symbol)
+    
   }
 })();
