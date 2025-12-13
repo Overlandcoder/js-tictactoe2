85ed20cffd2c83511d378404219ba0a58c789a67
Overlander <anuragnandal@hotmail.com>
2025-12-12 20:19:03 -0500

    Add ability to check if game has been won

--- script.js
+++ script.js
@@ -4,6 +4,52 @@ Array.prototype.random = function () {
 
 const gameBoard = (function () {
   const boardArray = [];
+
+  function addSymbol(index, symbol) {
+    boardArray[index] = symbol;
+  }
+
+  const isBoardFull = () => boardArray.length === 9;
+  const symbolAt = index => boardArray.at(index);
+
+  return { boardArray, addSymbol, isBoardFull, symbolAt };
+})();
+
+function createPlayer(name, symbol) {
+  return { name, symbol };
+}
+
+const gameDisplay = (function () {
+  const gameStatusDiv = document.querySelector("#game-status");
+
+  function announceSymbols(player1Symbol, player2Symbol) {
+    const symbolsInfoDiv = document.querySelector("#symbols-info");
+    symbolsInfoDiv.textContent = `Player 1 is ${player1Symbol},
+                                  Player 2 is ${player2Symbol}.`;
+  }
+
+  function announceTurn(playerName) {
+    gameStatusDiv.textContent = `${playerName}, it's your turn.`;
+  }
+
+  function displaySymbol(cell, symbol) {
+    cell.textContent = symbol;
+  }
+
+  function announceWinner(playerName) {
+    gameStatusDiv.textContent = `${playerName} has won!`;
+  }
+
+  return { announceSymbols, announceTurn, displaySymbol, announceWinner };
+})();
+
+const game = (function () {
+  const player1 = createPlayer("Player 1");
+  const player2 = createPlayer("Player 2");
+  const assignOtherSymbol = (symbol) => (symbol === "X" ? "O" : "X");
+  const playButton = document.querySelector("#play");
+  playButton.addEventListener("click", play);
+  let currentPlayer;
   const WINNING_COMBOS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -14,46 +60,6 @@ const gameBoard = (function () {
     [0, 4, 8],
     [2, 4, 6],
   ];
-
-  function addSymbol(index, symbol) {
-    boardArray[index] = symbol;
-  }
-
-  const isBoardFull = () => boardArray.length === 9;
-
-  return { boardArray, addSymbol, isBoardFull };
-})();
-
-function createPlayer(name, symbol) {
-  return { name, symbol };
-}
-
-const gameDisplay = (function () {
-  function announceSymbols(player1Symbol, player2Symbol) {
-    const symbolsInfoDiv = document.querySelector("#symbols-info");
-    symbolsInfoDiv.textContent = `Player 1 is ${player1Symbol},
-                                  Player 2 is ${player2Symbol}.`;
-  }
-
-  function announceTurn(playerName) {
-    const turnInfoDiv = document.querySelector("#turn-info");
-    turnInfoDiv.textContent = `${playerName}, it's your turn.`;
-  }
-
-  function displaySymbol(cell, symbol) {
-    cell.textContent = symbol;
-  }
-
-  return { announceSymbols, announceTurn, displaySymbol };
-})();
-
-const game = (function () {
-  const player1 = createPlayer("Player 1");
-  const player2 = createPlayer("Player 2");
-  const assignOtherSymbol = (symbol) => (symbol === "X" ? "O" : "X");
-  const playButton = document.querySelector("#play");
-  playButton.addEventListener("click", play);
-  let currentPlayer;
 
   const assignRandomSymbol = () => ["X", "O"].random();
 
@@ -82,7 +88,24 @@ const game = (function () {
   function handleMove(cell) {
     gameBoard.addSymbol(cell.id, currentPlayer.symbol)
     gameDisplay.displaySymbol(cell, currentPlayer.symbol);
+    if (isGameOver()) return;
     currentPlayer = switchTurn();
+    gameDisplay.announceTurn(currentPlayer.name);
+  }
+
+  function isGameOver() {
+    if (isGameWon()) {
+      gameDisplay.announceWinner(currentPlayer.name)
+      return true;
+    }
+  }
+
+  function isGameWon() {
+    return WINNING_COMBOS.some((combo) => areAllSameSymbol(combo))
+  }
+
+  function areAllSameSymbol(combo) {
+    return combo.every(index => gameBoard.symbolAt(index) === currentPlayer.symbol)
   }
 
   const switchTurn = () => (currentPlayer === player1 ? player2 : player1);
